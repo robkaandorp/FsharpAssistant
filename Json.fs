a665f97bb66f25ef11f839f739caebfd3bb8134a
Rob Kaandorp <rob@di.nl>
2025-02-10 09:18:14 +0100

    Fix json deserialization

--- Json.fs
+++ Json.fs
@@ -4,7 +4,6 @@ open System.Text.Json
 open System.Text.Json.Serialization
 
 let options = JsonSerializerOptions()
-options.Converters.Add(JsonFSharpConverter())
 
 let fromJson<'T> (text: string): 'T =
     JsonSerializer.Deserialize<'T>(text, options)
